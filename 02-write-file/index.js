bbb76414571654e61b038686e438fae8f960d140
romanbabok <roman.babok18@gmail.com>
2022-05-23 23:51:17 +0300

    feat: add the solution of 02-task

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const readline = require('node:readline');
+const {stdin, stdout} = require('node:process');
+const currPath = path.join(__dirname, 'text.txt');
+
+const stream = fs.createWriteStream(currPath);
+const rl = readline.createInterface({input: stdin, output: stdout});
+
+rl.on('SIGINT', () => {
+  console.log('Файл text.txt создан!');
+  rl.close();
+});
+
+console.log('Напишите что нибудь в консоль:');
+rl.on('line', (line) => {
+  if (line === 'exit' || line === 'Exit') {
+    console.log('Файл text.txt создан!');
+    rl.close();
+  } else {
+    stream.write(`${line}\n`);
+  }
+});
